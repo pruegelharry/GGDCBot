e6bc8c00d17bedd66628d877aba9a5bb770d1c25
jankuepper <janluca0000046@gmail.com>
2025-01-06 13:11:19 +0100

    chore: minor cleanup

--- src/sentry/index.js
+++ src/sentry/index.js
@@ -1,21 +1,11 @@
-import {init} from "@sentry/node";
-import { nodeProfilingIntegration } from '@sentry/profiling-node';
+import { init } from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
-const SENTRY_DSN = process.env.SENTRY_DSN
-console.log(SENTRY_DSN)
-// Ensure to call this before importing any other modules!
+const SENTRY_DSN = process.env.SENTRY_DSN;
 init({
   dsn: `${SENTRY_DSN}`,
-  integrations: [
-    // Add our Profiling integration
-    nodeProfilingIntegration(),
-  ],
-
-  // Add Tracing by setting tracesSampleRate
-  // We recommend adjusting this value in production
+  integrations: [nodeProfilingIntegration()],
   tracesSampleRate: 1.0,
-
-  // Set sampling rate for profiling
   // This is relative to tracesSampleRate
   profilesSampleRate: 1.0,
 });
